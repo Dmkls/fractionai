3d54a2e828021a55c1f5ccb42de2d8ee663c0b65
Mbiphes <castlegima@gmail.com>
2024-12-27 01:32:26 +1000

    add threats

--- main.py
+++ main.py
@@ -1,32 +1,54 @@
 import asyncio
 import aiohttp
 import fake_useragent
-import aiofiles
-from colorama import Fore, Style
+from colorama import Fore
+from random import randint
 
+THREATS = 10
+THREATS_DELAY = (1, 30) # delay in seconds before account processing starts
 URL: str = "https://backend.fractionai.xyz/api/waitList/createWaitlist"
+with open("emails.txt", 'r+') as f:
+    emails = list(filter(bool, f.read().splitlines()))  # file to list
+emails_total = len(emails)
 
-async def read_emails():
-    tasks = []  
-    async with aiofiles.open("emails.txt", mode='r') as f:
-        async for line in f:
-            email = line.split(":")[0].strip()
-            ua = fake_useragent.UserAgent().random
-            header = {'User-Agent': ua}
-            tasks.append(add_email_to_waitlist(email, header))
+print(Fore.RED + f"Creating user agents, please wait")
+user_agents = [fake_useragent.UserAgent().random for _ in range(emails_total)]
+print(Fore.GREEN + f"User agents were successfully created")
 
-    await asyncio.gather(*tasks)
+def hello_message():
+    print(Fore.GREEN + f"Found {emails_total} email/s to process\n"
+                       f"Starting..\n")
 
-async def add_email_to_waitlist(email: str, header: dict):
+
+def create_tasks():
+    semaphore = asyncio.Semaphore(THREATS)
+    tasks = [process_emails(email, ua, semaphore) for email, ua in zip(emails, user_agents)]
+    return tasks
+
+
+async def add_email_to_waitlist(email: str, header: dict) -> bool:
+    await asyncio.sleep(randint(THREATS_DELAY[0], THREATS_DELAY[1]))
     async with aiohttp.ClientSession() as session:
         async with session.post(URL, json={'email': email}, headers=header) as response:
             if response.status == 201:
                 print(Fore.GREEN + f"{email} - was registered successfully!")
+                return True
             else:
                 print(Fore.RED + f"{email} - error occurred while sending a request.")
+                return False
+
+
+async def process_emails(email: str, ua: str, semaphore: asyncio.Semaphore):
+    async with semaphore:
+        if not await add_email_to_waitlist(email, {'User-Agent': ua,}):
+            with open('failed_emails.txt', 'a') as failed:
+                failed.write(f"{email}\n")
+
 
 async def main():
-    await read_emails()
+    hello_message()
+    await asyncio.gather(*create_tasks())
+
 
 if __name__ == "__main__":
     asyncio.run(main())
